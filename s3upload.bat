2c24bcb8b66f758908db80e4dc64e734d6333ec1
fenceFoil <karnavasbj@gmail.com>
2021-07-26 18:44:47 -0400

    First version!

---
+++ s3upload.bat
@@ -0,0 +1,2 @@
+aws s3 cp dist/ s3://www.williamkarnavas.com/supermarkov/ --recursive
+
--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="app">
-    <div 
-      id="splashscreen" 
-      class="gentlebg" 
-      :style="{display:(showSplash)?'':'none'}"
-      >
-      <div id="splashGraph">
+    <div
+      id="splashscreen"
+      class="gentlebg"
+      :style="{ display: showSplash ? '' : 'none' }"
+    >
+      <div id="splashGraph"></div>
+      <div style="z-index: 20000;pointer-events: none; ">
+        Clips Loaded: {{ Math.min(loadedShots, shotNames.length) }} of
+        {{ shotNames.length }}
       </div>
       <div style="z-index: 20000">
-        Clips Loaded: {{Math.min(loadedShots, shotNames.length)}} of {{shotNames.length}}
-      </div>  
-      <div style="z-index: 20000">
         <a href="#" @click="clearSplashscreen()">Begin</a>
-      </div> 
+      </div>
     </div>
     <!--div style="display:none">
       <video controls v-for="shot in shotNames" :key="shot" preload="auto" @canplaythrough="incrementReady()">
@@ -26,31 +26,30 @@
       <!-- :style="{display:(currShot == shot)?'':'none'}" -->
       <div style="position:relative;width: min(100vw, 990px);">
         <video
-          v-for="shot in shotNames" :key="shot" 
-          preload="auto" 
+          v-for="shot in shotNames"
+          :key="shot"
+          preload="auto"
           @canplaythrough="incrementReady()"
-          :style="{zIndex:(currShot == shot)?'100':'10'}"
-          :id="`video-${shot}`" 
+          :style="{ zIndex: currShot == shot ? '100' : '10' }"
+          :id="`video-${shot}`"
           :src="`shots/${shot}.mp4`"
           @ended="queueUpNextShot(shot)"
           style="width: min(100vw, 990px);position:absolute;top:0;left:0;"
-          />
-        <img src="shots/thumbnails/Adoors-24.jpg">
+        />
+        <img src="shots/thumbnails/Adoors-24.jpg" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import _ from 'lodash';
-import cytoscape from 'cytoscape';
+import _ from "lodash";
+import cytoscape from "cytoscape";
 
 export default {
-  name: 'App',
-  components: {
-  },
-  computed: {
-  },
+  name: "App",
+  components: {},
+  computed: {},
   data() {
     return {
       showSplash: true,
@@ -130,316 +129,323 @@ export default {
       ],
       authenticMarkovTransitions: [
         {
-          from:"AB",
-          to:"Btalks",
+          from: "AB",
+          to: "Btalks",
         },
         {
-          from:"AB",
-          to:"Btalks",
+          from: "AB",
+          to: "Btalks",
         },
         {
-          from:"AB",
-          to:"Btalks",
+          from: "AB",
+          to: "Btalks",
         },
         {
-          from:"AB",
-          to:"Btalks",
+          from: "AB",
+          to: "Btalks",
         },
         {
-          from:"AB",
-          to:"Btalks",
+          from: "AB",
+          to: "Btalks",
         },
         {
-          from:"AB",
-          to:"Btalks",
+          from: "AB",
+          to: "Btalks",
         },
         {
-          from:"AB",
-          to:"CexitsRight",
+          from: "AB",
+          to: "CexitsRight",
         },
         {
-          from:"AB",
-          to:"Cstare",
+          from: "AB",
+          to: "Cstare",
         },
         {
-          from:"AB",
-          to:"Czoom",
+          from: "AB",
+          to: "Czoom",
         },
         {
-          from:"ABindoors",
-          to:"Adoors",
+          from: "ABindoors",
+          to: "Adoors",
         },
         {
-          from:"Adoors",
-          to:"AwalkingLR",
+          from: "Adoors",
+          to: "AwalkingLR",
         },
         {
-          from:"Aenters",
-          to:"Czoom",
+          from: "Aenters",
+          to: "Czoom",
         },
         {
-          from:"AentersYells",
-          to:"AtalksCries",
+          from: "AentersYells",
+          to: "AtalksCries",
         },
         {
-          from:"AexitBaloneClose",
-          to:"AexitBaloneFar",
+          from: "AexitBaloneClose",
+          to: "AexitBaloneFar",
         },
         {
-          from:"AexitBaloneFar",
-          to:"Btalks",
+          from: "AexitBaloneFar",
+          to: "Btalks",
         },
         {
-          from:"AkneelingBenters",
-          to:"BtalksDown",
+          from: "AkneelingBenters",
+          to: "BtalksDown",
         },
         {
-          from:"AlookingBack",
-          to:"AB",
+          from: "AlookingBack",
+          to: "AB",
         },
         {
-          from:"Astanding",
-          to:"Asun",
+          from: "Astanding",
+          to: "Asun",
         },
         {
-          from:"AstandsIntoAB",
-          to:"Btalks",
+          from: "AstandsIntoAB",
+          to: "Btalks",
         },
         {
-          from:"Asun",
-          to:"Aenters",
+          from: "Asun",
+          to: "Aenters",
         },
         {
-          from:"Atalks",
-          to:"AB",
+          from: "Atalks",
+          to: "AB",
         },
         {
-          from:"Atalks",
-          to:"Btalks",
+          from: "Atalks",
+          to: "Btalks",
         },
         {
-          from:"Atalks",
-          to:"Btalks",
+          from: "Atalks",
+          to: "Btalks",
         },
         {
-          from:"Atalks",
-          to:"Btalks",
+          from: "Atalks",
+          to: "Btalks",
         },
         {
-          from:"Atalks",
-          to:"Btalks",
+          from: "Atalks",
+          to: "Btalks",
         },
         {
-          from:"Atalks",
-          to:"Btalks",
+          from: "Atalks",
+          to: "Btalks",
         },
         {
-          from:"Atalks",
-          to:"Cblink",
+          from: "Atalks",
+          to: "Cblink",
         },
         {
-          from:"Atalks",
-          to:"Cblink",
+          from: "Atalks",
+          to: "Cblink",
         },
         {
-          from:"Atalks",
-          to:"Cclose",
+          from: "Atalks",
+          to: "Cclose",
         },
         {
-          from:"Atalks",
-          to:"Cclose",
+          from: "Atalks",
+          to: "Cclose",
         },
         {
-          from:"Atalks",
-          to:"Czoom",
+          from: "Atalks",
+          to: "Czoom",
         },
         {
-          from:"AtalksBbackground",
-          to:"Btalks",
+          from: "AtalksBbackground",
+          to: "Btalks",
         },
         {
-          from:"AtalksCries",
-          to:"AtalksCriesIntoKneeling",
+          from: "AtalksCries",
+          to: "AtalksCriesIntoKneeling",
         },
         {
-          from:"AtalksCriesIntoKneeling",
-          to:"AkneelingBenters",
+          from: "AtalksCriesIntoKneeling",
+          to: "AkneelingBenters",
         },
         {
-          from:"AtalksCriesKneeling",
-          to:"BtalksDown",
+          from: "AtalksCriesKneeling",
+          to: "BtalksDown",
         },
         {
-          from:"AtalksSide",
-          to:"Cfar",
+          from: "AtalksSide",
+          to: "Cfar",
         },
         {
-          from:"AtalksSide",
-          to:"Czoom",
+          from: "AtalksSide",
+          to: "Czoom",
         },
         {
-          from:"AwalkingLR",
-          to:"Astanding",
+          from: "AwalkingLR",
+          to: "Astanding",
         },
         {
-          from:"AzoomTears",
-          to:"END",
+          from: "AzoomTears",
+          to: "END",
         },
         {
-          from:"BrunningLR",
-          to:"AlookingBack",
+          from: "BrunningLR",
+          to: "AlookingBack",
         },
         {
-          from:"Btalks",
-          to:"AB",
+          from: "Btalks",
+          to: "AB",
         },
         {
-          from:"Btalks",
-          to:"AB",
+          from: "Btalks",
+          to: "AB",
         },
         {
-          from:"Btalks",
-          to:"AB",
+          from: "Btalks",
+          to: "AB",
         },
         {
-          from:"Btalks",
-          to:"AB",
+          from: "Btalks",
+          to: "AB",
         },
         {
-          from:"Btalks",
-          to:"AB",
+          from: "Btalks",
+          to: "AB",
         },
         {
-          from:"Btalks",
-          to:"AB",
+          from: "Btalks",
+          to: "AB",
         },
         {
-          from:"Btalks",
-          to:"AB",
+          from: "Btalks",
+          to: "AB",
         },
         {
-          from:"Btalks",
-          to:"AentersYells",
+          from: "Btalks",
+          to: "AentersYells",
         },
         {
-          from:"Btalks",
-          to:"Atalks",
+          from: "Btalks",
+          to: "Atalks",
         },
         {
-          from:"Btalks",
-          to:"Atalks",
+          from: "Btalks",
+          to: "Atalks",
         },
         {
-          from:"Btalks",
-          to:"Atalks",
+          from: "Btalks",
+          to: "Atalks",
         },
         {
-          from:"Btalks",
-          to:"Atalks",
+          from: "Btalks",
+          to: "Atalks",
         },
         {
-          from:"Btalks",
-          to:"Atalks",
+          from: "Btalks",
+          to: "Atalks",
         },
         {
-          from:"Btalks",
-          to:"Atalks",
+          from: "Btalks",
+          to: "Atalks",
         },
         {
-          from:"Btalks",
-          to:"AtalksBbackground",
+          from: "Btalks",
+          to: "AtalksBbackground",
         },
         {
-          from:"Btalks",
-          to:"AzoomTears",
+          from: "Btalks",
+          to: "AzoomTears",
         },
         {
-          from:"BtalksDown",
-          to:"AstandsIntoAB",
+          from: "BtalksDown",
+          to: "AstandsIntoAB",
         },
         {
-          from:"BtalksDown",
-          to:"AtalksCriesKneeling",
+          from: "BtalksDown",
+          to: "AtalksCriesKneeling",
         },
         {
-          from:"Cblink",
-          to:"Atalks",
+          from: "Cblink",
+          to: "Atalks",
         },
         {
-          from:"Cblink",
-          to:"Btalks",
+          from: "Cblink",
+          to: "Btalks",
         },
         {
-          from:"Cclose",
-          to:"AtalksSide",
+          from: "Cclose",
+          to: "AtalksSide",
         },
         {
-          from:"Cclose",
-          to:"BrunningLR",
+          from: "Cclose",
+          to: "BrunningLR",
         },
         {
-          from:"CexitsRight",
-          to:"AexitBaloneClose",
+          from: "CexitsRight",
+          to: "AexitBaloneClose",
         },
         {
-          from:"Cfar",
-          to:"AtalksSide",
+          from: "Cfar",
+          to: "AtalksSide",
         },
         {
-          from:"Cstare",
-          to:"Atalks",
+          from: "Cstare",
+          to: "Atalks",
         },
         {
-          from:"Cstare",
-          to:"Btalks",
+          from: "Cstare",
+          to: "Btalks",
         },
         {
-          from:"Czoom",
-          to:"Atalks",
+          from: "Czoom",
+          to: "Atalks",
         },
         {
-          from:"Czoom",
-          to:"Atalks",
+          from: "Czoom",
+          to: "Atalks",
         },
         {
-          from:"Czoom",
-          to:"Atalks",
+          from: "Czoom",
+          to: "Atalks",
         },
         {
-          from:"Czoom",
-          to:"Cstare",
+          from: "Czoom",
+          to: "Cstare",
         },
         {
-          from:"START",
-          to:"Abindoors",
+          from: "START",
+          to: "Abindoors",
         },
       ],
-    }
+    };
   },
   methods: {
     clearSplashscreen: function() {
-      this.showSplash=false;
+      this.showSplash = false;
       this.queueUpNextShot();
     },
-    incrementReady: function () {
+    incrementReady: function() {
       this.loadedShots++;
     },
-    queueUpNextShot: function (caller=null) {      
+    queueUpNextShot: function(caller = null) {
       if (this.showSplash) {
         return;
       }
-      if ((caller != null) && (caller != this.currShot)) {
+      if (caller != null && caller != this.currShot) {
         return;
       }
       let lastShotVideo = document.getElementById(`video-${this.currShot}`);
 
       // Given current shot, select a random markov transition and note the to shot
-      let nextShotBaseName = _.sample(_.filter(this.authenticMarkovTransitions, (transition) => (transition.from == this.currShot.split('-')[0]))).to;
-      if (nextShotBaseName == 'END') {
-        nextShotBaseName = 'ABindoors';
+      let nextShotBaseName = _.sample(
+        _.filter(
+          this.authenticMarkovTransitions,
+          (transition) => transition.from == this.currShot.split("-")[0]
+        )
+      ).to;
+      if (nextShotBaseName == "END") {
+        nextShotBaseName = "ABindoors";
       }
 
-      let nextShot = _.sample(_.filter(this.shotNames, (shot) => (shot.startsWith(nextShotBaseName))));
+      let nextShot = _.sample(
+        _.filter(this.shotNames, (shot) => shot.startsWith(nextShotBaseName))
+      );
       let shotVideo = document.getElementById(`video-${nextShot}`);
       shotVideo.currentTime = 0;
       shotVideo.play();
@@ -447,95 +453,83 @@ export default {
 
       // Cleanup!
       lastShotVideo.currentTime = 0;
-
-      // Get frames of selected destination shot, and use that to time the next transition
-      /*if (this.currShot != undefined) {
-        setTimeout(() => {
-          this.queueUpNextShot();
-        }, this.currShot.split('-')[1]*(1000.0/30.0));
-        console.log(this.currShot.split('-')[1]*(1000.0/30.0))
-      }*/
     },
   },
   mounted() {
-    let elements = []
-
+    let elements = [];
 
     // Some ECMAScript 2017 goodness
-    function noDupes2(...args){
-      return [].concat(...args).filter((v, i, arr) => arr.indexOf(v)==i);
+    function noDupes2(...args) {
+      return [].concat(...args).filter((v, i, arr) => arr.indexOf(v) == i);
     }
 
-    noDupes2(this.shotNames.map(x => x.split('-')[0])).forEach((shotName) => {
-      elements.push(
-        {
-          group: 'nodes',
-          data: {
-            id: shotName
-          },
-          position: {
-            x: Math.random()*150,
-            y: Math.random()*150
-          }
-        }
-      )
-    })
-/*
+    noDupes2(this.shotNames.map((x) => x.split("-")[0])).forEach((shotName) => {
+      elements.push({
+        group: "nodes",
+        data: {
+          id: shotName,
+        },
+      });
+    });
+
     this.authenticMarkovTransitions.forEach((transition, i) => {
-      if (transition.to != 'END' && transition.from != 'START') {
-        elements.push(
-          {
-            group: 'edges',
-            data: {
-              id: `edge${i}`,
-              source: transition.from,
-              target: transition.to
-            }
-          }
-        )
+      if (transition.to != "END" && transition.from != "START") {
+        elements.push({
+          group: "edges",
+          data: {
+            id: `edge${i}`,
+            source: transition.from,
+            target: transition.to,
+          },
+        });
       }
-    })*/
+    });
 
     let cy = cytoscape({
-        wheelSensitivity: 0.3,
-        userZoomingEnabled: false,
-        userPanningEnabled: false,
-        boxSelectionEnabled: false,
-        autoungrabify: true,
-        autolock: true,
-        container: document.getElementById('splashGraph'),
-        elements: elements,
-        style: [
-          {
-            selector: 'node',
-            style: {
-              'background-color': '#F5876F',
-              'width': '5',
-              'height': '5'
-            }
+      wheelSensitivity: 0.3,
+      /*userZoomingEnabled: false,
+      userPanningEnabled: false,
+      boxSelectionEnabled: false,
+      autoungrabify: true,
+      autolock: true,*/
+      container: document.getElementById("splashGraph"),
+      elements: elements,
+      style: [
+        {
+          selector: "node",
+          style: {
+            "background-color": "#F5876F",
+            width: "5",
+            height: "5",
           },
+        },
 
-          {
-            selector: 'edge',
-            style: {
-              'width': 3,
-              'line-color': '#F5876F',
-              'target-arrow-color': '#F5876F',
-              'target-arrow-shape': 'triangle',
-              'curve-style': 'bezier'
-            }
-          }
-        ],
-        layout: {
-          name: 'cose',
-          animate: true,
-          padding: 50,
-        }
-      });
+        {
+          selector: "edge",
+          style: {
+            width: 0.8,
+            "line-color": "#F7A9A8",
+            "target-arrow-color": "#F2A58F",
+            "target-arrow-shape": "triangle",
+            "curve-style": "straight",
+            "line-dash-pattern": [1, 1],
+            "arrow-scale": 0.6,
+          },
+        },
+      ],
+      //layout: ,
+    });
+
+    cy.elements().layout({
+      name: "cose",
+      fit: true,
+      animate: false,
+      padding: 50,
+      randomize: true,
+    }).run();
   },
-}
+};
 </script>
-
 
 <style lang="scss">
 #app {
@@ -546,19 +540,19 @@ export default {
 
   display: flex;
   place-items: center;
-  overflow:hidden;
+  overflow: hidden;
 }
 
 #splashscreen {
-  position:absolute;
-  top:0;
-  left:0;
-  width:100%;
-  height:100%;
-  z-index:10000;
-  display:flex;
-  flex-direction:column;
-  justify-content:space-evenly;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 10000;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
   font-size: 4em;
 }
 
@@ -572,22 +566,25 @@ export default {
 
 #splashGraph {
   position: absolute;
-  top:0;
-  left:0;
-  width:100vw;
+  top: 0;
+  left: 0;
+  width: 100vw;
   height: 100vh;
   z-index: 10001;
 }
 
 // I guess I'm bugfixing cytoscape.js now
 #splashGraph div canvas {
-  left:0;
+  left: 0;
 }
 
 .gentlebg {
-  background: rgb(229,135,76);
-  background: linear-gradient(125deg, rgba(229,135,76,1) 0%, rgba(245,203,135,1) 33%, rgba(253,210,184,1) 100%); 
-
+  background: rgb(229, 135, 76);
+  background: linear-gradient(
+    125deg,
+    rgba(229, 135, 76, 1) 0%,
+    rgba(245, 203, 135, 1) 33%,
+    rgba(253, 210, 184, 1) 100%
+  );
 }
-
 </style>
