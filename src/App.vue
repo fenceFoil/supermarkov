3d098504b471c5766069dc3922b0b4938e63790e
fenceFoil <karnavasbj@gmail.com>
2021-07-27 23:40:42 -0400

    Add music, remove excess video elements and resources and thumbnails, remove 9 frame Czoom, add time faded to black after cryingzoom, fix starting on doors instead of inside, add mute option, fix layout of all screens on mobile. That's all.

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,27 @@
 <template>
   <div id="app">
-    <div
-      id="splashscreen"
-      class="gentlebg"
-      :style="{ display: showSplash ? '' : 'none' }"
-    >
+    <div id="splashscreen" class="gentlebg" v-if="showSplash">
       <div id="splashGraph"></div>
-      <div style="z-index: 20000;pointer-events: none; ">
-        Clips Loaded: {{ Math.min(loadedShots, shotNames.length) }} of
-        {{ shotNames.length }}
+      <div style="z-index: 20000;pointer-events: none; text-shadow: 3px 3px 3px black;">
+        Super Markov Robot Solar 123: <br><i>An Infinite Drama</i>
       </div>
       <div style="z-index: 20000">
-        <a href="#" @click="clearSplashscreen()">Begin</a>
+        <div style="display:flex;flex-direction:column;">
+          <a style="display: block;margin-bottom:60px" v-if="videoLoaded" href="#" @click="clearSplashscreen()">Begin 🔈</a>
+          <a style="display: block;" v-if="videoLoaded" href="#" @click="clearSplashscreen(true)">Begin 🔊</a>
+          <template v-else>Loading video...</template>
+        </div>
       </div>
     </div>
-    <!--div style="display:none">
-      <video controls v-for="shot in shotNames" :key="shot" preload="auto" @canplaythrough="incrementReady()">
-        <source :src="`shots/${shot}.mp4`" type="video/mp4" >
-      </video>
-    </div-->
-    <div style="width:100vw;display:flex;justify-content:center;">
-      <!--<img :src="`shots/thumbnails/${currShot}.jpg`"-->
-      <!--video controls preload="auto" autoplay :src="`shots/${currShot}.mp4`">
-      </video-->
-      <!-- :style="{display:(currShot == shot)?'':'none'}" -->
-      <div style="position:relative;width: min(100vw, 990px);">
+    <div style="width:100vw;height:100vh;display:flex;justify-content:center;align-items:center;">
+      <audio src="audioblocks-the-endgame_BnG_uQ_Xw-SBA-300504840.mp3" id="dramaticMusic" loop style="height:0;width:0;" /> 
+      <div style="position:relative;width: min(100vw, 990px);height:720px;z-index:2;">
         <video
-          v-for="shot in shotNames"
-          :key="shot"
+          muted
           preload="auto"
-          @canplaythrough="incrementReady()"
-          :style="{ zIndex: currShot == shot ? '100' : '10' }"
-          :id="`video-${shot}`"
-          :src="`shots/${shot}.mp4`"
-          @ended="queueUpNextShot(shot)"
-          style="width: min(100vw, 990px);position:absolute;top:0;left:0;"
+          id="dramaclip"
+          style="width: min(100vw, 990px);height:720px;position:absolute;top:0;left:0;"
         />
-        <img src="shots/thumbnails/Adoors-24.jpg" />
       </div>
     </div>
   </div>
@@ -53,8 +38,8 @@ export default {
   data() {
     return {
       showSplash: true,
-      currShot: "ABindoors-125",
-      loadedShots: 0,
+      currShot: "START",
+      videoLoaded: 0,
       shotNames: [
         "ABindoors-125",
         "Adoors-24",
@@ -65,8 +50,8 @@ export default {
         "Czoom-41",
         "Atalks-89",
         "Cclose-47",
-        "AtalksSide-108",
-        "Cfar-7",
+        "AtalksSide-68",
+        "Cfar-47",
         "AtalksSide-70",
         "Czoom-33",
         "Atalks-28",
@@ -84,7 +69,6 @@ export default {
         "AtalksBbackground-22",
         "Btalks-14",
         "AB-31",
-        "Czoom-9",
         "Cstare-26",
         "Btalks-42b",
         "AB-87",
@@ -117,7 +101,7 @@ export default {
         "AexitBaloneFar-21",
         "Btalks-19",
         "AentersYells-49",
-        "AtalksCries-35",
+        "AtalksCries-34",
         "AtalksCriesIntoKneeling-14",
         "AkneelingBenters-30",
         "BtalksDown-16",
@@ -128,6 +112,10 @@ export default {
         "AzoomTears-84",
       ],
       authenticMarkovTransitions: [
+        {
+          from: "START",
+          to: "ABindoors-125",
+        },
         {
           from: "AB",
           to: "Btalks",
@@ -410,18 +398,296 @@ export default {
         },
         {
           from: "START",
-          to: "Abindoors",
+          to: "ABindoors",
         },
       ],
+      shotTimings: {
+          "ABindoors-125": {
+            startFrame: 1,
+            numFrames: 125
+          },
+"Adoors-24": {
+            startFrame: 126,
+            numFrames: 24
+          },
+"AwalkingLR-46": {
+            startFrame: 150,
+            numFrames: 46
+          },
+"Astanding-53": {
+            startFrame: 196,
+            numFrames: 53
+          },
+"Asun-42": {
+            startFrame: 249,
+            numFrames: 42
+          },
+"Aenters-23": {
+            startFrame: 291,
+            numFrames: 23
+          },
+"Czoom-41": {
+            startFrame: 314,
+            numFrames: 41
+          },
+"Atalks-89": {
+            startFrame: 355,
+            numFrames: 89
+          },
+"Cclose-47": {
+            startFrame: 444,
+            numFrames: 47
+          },
+"AtalksSide-68": {
+            startFrame: 491,
+            numFrames: 68
+          },
+"Cfar-47": {
+            startFrame: 559,
+            numFrames: 47
+          },
+"AtalksSide-70": {
+            startFrame: 606,
+            numFrames: 70
+          },
+"Czoom-33": {
+            startFrame: 676,
+            numFrames: 33
+          },
+"Atalks-28": {
+            startFrame: 709,
+            numFrames: 28
+          },
+"Cblink-51": {
+            startFrame: 737,
+            numFrames: 51
+          },
+"Atalks-56a": {
+            startFrame: 788,
+            numFrames: 56
+          },
+"Czoom-57": {
+            startFrame: 844,
+            numFrames: 57
+          },
+"Atalks-36": {
+            startFrame: 901,
+            numFrames: 36
+          },
+"Cclose-42": {
+            startFrame: 937,
+            numFrames: 42
+          },
+"BrunningLR-27": {
+            startFrame: 979,
+            numFrames: 27
+          },
+"AlookingBack-20": {
+            startFrame: 1006,
+            numFrames: 20
+          },
+"AB-76": {
+            startFrame: 1026,
+            numFrames: 76
+          },
+"Btalks-27a": {
+            startFrame: 1102,
+            numFrames: 27
+          },
+"AB-14": {
+            startFrame: 1129,
+            numFrames: 14
+          },
+"Btalks-42a": {
+            startFrame: 1143,
+            numFrames: 42
+          },
+"AtalksBbackground-22": {
+            startFrame: 1185,
+            numFrames: 22
+          },
+"Btalks-14": {
+            startFrame: 1207,
+            numFrames: 14
+          },
+"AB-31": {
+            startFrame: 1221,
+            numFrames: 31
+          },
+"Cstare-26": {
+            startFrame: 1261,
+            numFrames: 26
+          },
+"Btalks-42b": {
+            startFrame: 1287,
+            numFrames: 42
+          },
+"AB-87": {
+            startFrame: 1329,
+            numFrames: 87
+          },
+"Cstare-31": {
+            startFrame: 1416,
+            numFrames: 31
+          },
+"Atalks-57": {
+            startFrame: 1447,
+            numFrames: 57
+          },
+"Btalks-33": {
+            startFrame: 1504,
+            numFrames: 33
+          },
+"AB-62": {
+            startFrame: 1537,
+            numFrames: 62
+          },
+"Btalks-51a": {
+            startFrame: 1599,
+            numFrames: 51
+          },
+"Atalks-27": {
+            startFrame: 1650,
+            numFrames: 27
+          },
+"Btalks-76": {
+            startFrame: 1677,
+            numFrames: 76
+          },
+"AB-39": {
+            startFrame: 1753,
+            numFrames: 39
+          },
+"Btalks-51b": {
+            startFrame: 1792,
+            numFrames: 51
+          },
+"AB-35": {
+            startFrame: 1843,
+            numFrames: 35
+          },
+"Btalks-13a": {
+            startFrame: 1878,
+            numFrames: 13
+          },
+"Atalks-56b": {
+            startFrame: 1891,
+            numFrames: 56
+          },
+"Cblink-33": {
+            startFrame: 1947,
+            numFrames: 33
+          },
+"Btalks-27b": {
+            startFrame: 1980,
+            numFrames: 27
+          },
+"AB-38": {
+            startFrame: 2007,
+            numFrames: 38
+          },
+"Btalks-28": {
+            startFrame: 2045,
+            numFrames: 28
+          },
+"Atalks-110": {
+            startFrame: 2073,
+            numFrames: 110
+          },
+"Btalks-11": {
+            startFrame: 2183,
+            numFrames: 11
+          },
+"Atalks-58": {
+            startFrame: 2194,
+            numFrames: 58
+          },
+"Btalks-13b": {
+            startFrame: 2252,
+            numFrames: 13
+          },
+"Atalks-83": {
+            startFrame: 2265,
+            numFrames: 83
+          },
+"Btalks-24": {
+            startFrame: 2348,
+            numFrames: 24
+          },
+"Atalks-13": {
+            startFrame: 2372,
+            numFrames: 13
+          },
+"AB-33": {
+            startFrame: 2385,
+            numFrames: 33
+          },
+"CexitsRight-26": {
+            startFrame: 2418,
+            numFrames: 26
+          },
+"AexitBaloneClose-17": {
+            startFrame: 2444,
+            numFrames: 17
+          },
+"AexitBaloneFar-21": {
+            startFrame: 2461,
+            numFrames: 21
+          },
+"Btalks-19": {
+            startFrame: 2482,
+            numFrames: 19
+          },
+"AentersYells-49": {
+            startFrame: 2501,
+            numFrames: 49
+          },
+"AtalksCries-34": {
+            startFrame: 2550,
+            numFrames: 34
+          },
+"AtalksCriesIntoKneeling-14": {
+            startFrame: 2585,
+            numFrames: 14
+          },
+"AkneelingBenters-30": {
+            startFrame: 2599,
+            numFrames: 30
+          },
+"BtalksDown-16": {
+            startFrame: 2629,
+            numFrames: 16
+          },
+"AtalksCriesKneeling-53": {
+            startFrame: 2645,
+            numFrames: 53
+          },
+"BtalksDown-31": {
+            startFrame: 2698,
+            numFrames: 31
+          },
+"AstandsIntoAB-68": {
+            startFrame: 2729,
+            numFrames: 68
+          },
+"Btalks-80": {
+            startFrame: 2797,
+            numFrames: 80
+          },
+"AzoomTears-84": {
+            startFrame: 2877,
+            numFrames: 84
+          },
+      },
     };
   },
   methods: {
-    clearSplashscreen: function() {
+    clearSplashscreen: function(playMusic = false) {
       this.showSplash = false;
       this.queueUpNextShot();
-    },
-    incrementReady: function() {
-      this.loadedShots++;
+      if (playMusic) {
+        document.getElementById('dramaticMusic').play();
+      }
     },
     queueUpNextShot: function(caller = null) {
       if (this.showSplash) {
@@ -430,7 +696,6 @@ export default {
       if (caller != null && caller != this.currShot) {
         return;
       }
-      let lastShotVideo = document.getElementById(`video-${this.currShot}`);
 
       // Given current shot, select a random markov transition and note the to shot
       let nextShotBaseName = _.sample(
@@ -442,20 +707,36 @@ export default {
       if (nextShotBaseName == "END") {
         nextShotBaseName = "ABindoors";
       }
+      console.log(nextShotBaseName)
 
       let nextShot = _.sample(
         _.filter(this.shotNames, (shot) => shot.startsWith(nextShotBaseName))
       );
-      let shotVideo = document.getElementById(`video-${nextShot}`);
-      shotVideo.currentTime = 0;
-      shotVideo.play();
-      this.currShot = nextShot;
+      let nextShotStartTime = (this.shotTimings[nextShot].startFrame-1) * (1/29.97);
+      let videoElement = document.getElementById('dramaclip');
+      videoElement.currentTime = nextShotStartTime;
+      videoElement.play();
 
-      // Cleanup!
-      lastShotVideo.currentTime = 0;
+      let numFrames = this.shotTimings[nextShot].numFrames-1;
+
+      if (nextShot == 'AzoomTears-84') {
+        // Let fade to black play through properly
+        numFrames = 250;
+      }
+
+      let that = this;
+      setTimeout(() => {
+        that.queueUpNextShot();
+      }, (numFrames) * (1000/29.97));
+
+      this.currShot = nextShot;
     },
   },
-  mounted() {
+  async mounted() {
+    // Validate data all matches
+    console.log(_.difference(this.shotNames, Object.keys(this.shotTimings)))
+    console.log(_.difference(Object.keys(this.shotTimings), this.shotNames))
+
     let elements = [];
 
     // Some ECMAScript 2017 goodness
@@ -527,11 +808,17 @@ export default {
       padding: 50,
       randomize: true,
     }).run();
+
+    // Download video
+    let videoFile = await (await fetch('dramaclip.mp4')).blob();
+    this.videoLoaded = true;
+    document.getElementById('dramaclip').src = URL.createObjectURL(videoFile);
   },
 };
 </script>
 
 <style lang="scss">
+
 #app {
   background-color: #222;
   height: 100vh;
@@ -547,13 +834,14 @@ export default {
   position: absolute;
   top: 0;
   left: 0;
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
   z-index: 10000;
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
-  font-size: 4em;
+  font-size: 5vw;
+
 }
 
 #splashscreen a {
